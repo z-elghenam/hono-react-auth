4c0823436861e5ae48e79fc710ca2700869499c7
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-22 16:16:10 +0000

    feat: Integrate auth routes and todos routes into main app

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
+import { auth } from "./lib/auth";
+import { todos } from "./routes/todo.routes";
 
 const app = new Hono();
 
 const route = app
-  .get("/", (c) => {
-    return c.text("Hello Hono!");
-  })
-  .get("/api/people", (c) => {
+  .on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
+  .route("/api/todos", todos)
+  .get("/people", (c) => {
     return c.json([
       { id: 1, name: "Alice" },
       { id: 2, name: "Bob" },
