c329e7801fff1ef74b7d388043fda722402f73be
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-22 15:52:36 +0000

    feat: add authentication setup with better-auth and drizzle adapter.

---
+++ server/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/db";
+import * as schema from "@/db/schema";
+import { openAPI } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: [process.env.CLIENT_URL!],
+  plugins: [openAPI()],
+});
