f451b690582d2c134a57e876c71da3628763efbf
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-25 23:12:05 +0000

    feat: add static file serving and request logging middleware

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,16 @@
 import { Hono } from "hono";
 import { auth } from "./lib/auth";
 import { todos } from "./routes/todo.routes";
+import { serveStatic } from "hono/bun";
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
 const route = app
+  .use(logger())
+  .use("/*", serveStatic({ root: "./client/dist" }))
   .on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
-  .route("/api/todos", todos)
-  .get("/people", (c) => {
-    return c.json([
-      { id: 1, name: "Alice" },
-      { id: 2, name: "Bob" },
-      { id: 3, name: "Charlie" },
-    ]);
-  });
+  .route("/api/todos", todos);
 
 export type AppType = typeof route;
 
