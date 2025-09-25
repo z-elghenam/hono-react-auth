f7b55657332faecdc19210e37d98a0beec51a1d3
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-25 21:31:21 +0000

    feat: add three functions for insert and update and delete todo from todos table

--- server/db/queries.ts
+++ server/db/queries.ts
@@ -1,6 +1,7 @@
-import { desc, eq } from "drizzle-orm";
 import { db } from "./db";
 import { todos } from "./schema";
+import { eq, desc, and } from "drizzle-orm";
+import type { NewTodo, Todo } from "../types";
 
 export const getTodosByUserId = async (userId: string) => {
   return await db
@@ -8,4 +9,33 @@ export const getTodosByUserId = async (userId: string) => {
     .from(todos)
     .where(eq(todos.userId, userId))
     .orderBy(desc(todos.createdAt));
+};
+
+export const insertTodo = async (todo: NewTodo) => {
+  const [result] = await db.insert(todos).values(todo).returning();
+
+  return result;
+};
+
+export const deleteTodoById = async (id: string, userId: string) => {
+  const result = await db
+    .delete(todos)
+    .where(and(eq(todos.id, id), eq(todos.userId, userId)))
+    .returning();
+
+  return result.length > 0;
+};
+
+export const updateTodoById = async (
+  id: string,
+  userId: string,
+  updates: Partial<NewTodo>
+): Promise<Todo | undefined> => {
+  const [result] = await db
+    .update(todos)
+    .set(updates)
+    .where(and(eq(todos.id, id), eq(todos.userId, userId)))
+    .returning();
+
+  return result;
 };
